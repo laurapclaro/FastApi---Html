cca87364f3fa8b474fd42fc7a2b3fd08e0b5c15d
Laura <laurapapa693@gmail.com>
2025-10-27 10:59:32 -0300

    atualizações gerais

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 #pip install fastapi uvicorn jinja2
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 
+#rodar server
+# python -m uvicorn main:app --reload
 
-app = FastAPI("API de Alunos")
+app = FastAPI(title="API de Alunos")
 #config o diretorio dos templates jinja2
 templates = Jinja2Templates(directory="templates")
 
 
 #Pasta static para servir os arquivos (CSS, Imagens ou JV)
-app.mount("/static", StaticFiles(directory="static", name="static"))
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 alunos = [
     {"nome": "Felipe", "nota": 8.5},
     {"nome": "Laura", "nota": 9.0},
-    {"nome": "Yasmin", "nota": 8.0}
+    {"nome": "Yasmin", "nota": 8.0},
+    {"nome": "Kazuki", "nota": 5.0}
 ]
 
 
@@ -24,5 +27,18 @@ alunos = [
 @app.get("/", response_class=HTMLResponse)
 def exibir_alunos(request: Request):
     return templates.TemplateResponse(
-        "aluno.html", {"request": request, "Lista_alunos": alunos}
+        "alunos.html", {"request": request, "Lista_alunos": alunos}
     )
+
+@app.get("/cadastro", response_class=HTMLResponse)
+def cadastro_aluno(request: Request):
+    return templates.TemplateResponse(
+        "cadastro.html", {"request": request}
+    )
+
+#rota de cadastro
+
+@app.get("/cadastro")
+def salvar_aluno(nome: str = Form(...), nota: float = Form(...)):
+    alunos.append({ "nome": nome, "nota": nota})
+    return RedirectResponse(url="/", status_code=303)
